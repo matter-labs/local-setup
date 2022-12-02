8d8af408f160203dcef30f5e0a6f433f138356ee
Danil <deniallugo@gmail.com>
2022-12-02 14:06:02 +0100

    Add pull for start and down for clean
    Signed-off-by: Danil <deniallugo@gmail.com>

--- clear.sh
+++ clear.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+docker-compose down
 rm -rf ./volumes
--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 mkdir -p ./volumes
 mkdir -p ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
 
+docker-compose pull
 docker-compose up
 
