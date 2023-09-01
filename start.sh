f16f1843b5740ca8456432123badafa137daaa02
Matías Onorato <onoratomatias@gmail.com>
2023-09-01 11:50:48 +0200

    chore: Update to docker named volumes (#8)
    * Migrate from binds to volumes
    * Make volumes anonymous
    * Remove bind handling from scripts
    * Remove exposition of psql port
    * Update README.md
    
    ---------
    
    Co-authored-by: Joaquín P. Centeno <jpcenteno@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p ./volumes
-mkdir -p ./volumes/postgres ./volumes/geth ./volumes/zksync/env/dev ./volumes/zksync/data
-touch ./volumes/zksync/env.env
-
 docker-compose up
 
