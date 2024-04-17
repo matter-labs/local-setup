d90c36c49e0f76493be5cfd0343ca75e22bcb1c6
mm <mm@matterlabs.dev>
2024-04-17 11:50:44 +0200

    support custom names

--- start.sh
+++ start.sh
@@ -1,2 +1,32 @@
 #!/usr/bin/env bash
+# docker compose up -d
+
+# usage: ./start.sh INSTANCE_TYPE 
+# Instance type is specifying the docker image to take:
+# see https://hub.docker.com/r/matterlabs/local-node/tags for full list.
+# latest2.0 - is the 'main' one.
+
+INSTANCE_TYPE=${1:-latest2.0}
+
+export INSTANCE_TYPE=$INSTANCE_TYPE
 docker compose up -d
+
+
+check_all_services_healthy() {
+  service="zksync"
+  (docker compose ps $service | grep "(healthy)")
+  if [ $? -eq 0 ]; then
+    return 0
+  else
+    return 1  # If any service is not healthy, return 1
+  fi
+}
+
+
+# Loop until all services are healthy
+while ! check_all_services_healthy; do
+  echo "Services are not yet healthy, waiting..."
+  sleep 10  # Check every 10 seconds
+done
+
+echo "All services are healthy!"
